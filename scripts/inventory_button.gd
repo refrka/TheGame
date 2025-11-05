272ab1ac2c835093fc82d9de48b699a192748fe3
refkra <la.jernigan88@gmail.com>
2025-11-04 23:21:15 -0800

    f

--- scripts/inventory_button.gd
+++ scripts/inventory_button.gd
@@ -1,11 +1,14 @@
 extends Button
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	visible = false
+	GameStateHandler.character_started.connect(_on_character_started)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_character_started() -> void:
+	GameStateHandler.character.inventory.inventory_unlocked.connect(_on_inventory_unlocked)
+
+
+func _on_inventory_unlocked() -> void:
+	visible = true
--- scripts/skills_list.gd
+++ scripts/skills_list.gd
@@ -8,12 +8,14 @@ const skill_row = preload("res://scenes/skill_list_row.tscn")
 
 
 func _ready() -> void:
+	visible = false
 	Events.subscribe("add_skill", _on_add_skill_event)
 
 
 
 
 func _on_add_skill_event(event_data: Dictionary) -> void:
+	visible = true
 	var skill_data = event_data["skill_data"]
 	var row = skill_row.instantiate()
 	row.load_skill(skill_data)
